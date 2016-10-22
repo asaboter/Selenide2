af510a70a8aedf14c719b8bf69afde198176c9d8
user <user@user.com>
2016-10-22 08:23:28 -0700

    Second commit

---
+++ src/test/java/test/Barancev.java
@@ -0,0 +1,35 @@
+package test;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Description;
+
+import java.io.File;
+
+/**
+ * Created by monk on 10/22/2016.
+ */
+/*@Test
+public class Barancev {
+    public void rundriver() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Chrome_driver\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+        driver.close();
+
+    }
+
+
+}*/
+@Description("Uroki Baranceva")
+@Test
+public class Barancev {
+    public void rundriver1() {
+        ChromeDriverService service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File("C:\\Chrome_driver\\chromedriver.exe"))
+                .usingAnyFreePort()
+                .build();
+
+    }
+}
--- src/test/java/test/First.java
+++ src/test/java/test/First.java
@@ -1,19 +1,33 @@
+package test;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Description;
+import ru.yandex.qatools.allure.annotations.Features;
+import ru.yandex.qatools.allure.annotations.Stories;
+import ru.yandex.qatools.allure.annotations.Title;
 
 /**
  * Created by monk on 10/20/2016.
  */
+@Features("Google ")
+@Stories("Google closed")
 public class First {
 
 
-    //public static void main(String[] args) {
+
+
+    @Description("Test case check result of search on home page")
+    @Title("EG_003_Check result of search on home page")
+
     @Test
+
     public void rundriver() {
         System.setProperty("webdriver.chrome.driver", "C:\\Chrome_driver\\chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
         driver.get("http://www.google.com");
+        driver.close();
 
     }
 
